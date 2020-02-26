95b0b2e6582126a0782ac451e52d0cccd5dd4049
sarime <sarime@student.matnat.ifi.no>
2020-02-26 03:53:13 +0100

    Rekursjon part solved! Fuction converted to it's Recurtive version :)

--- Oblig2/src/main/Rekursjon.java
+++ Oblig2/src/main/Rekursjon.java
@@ -1,4 +1,25 @@
 package main;
 
 public class Rekursjon {
+
+    public static void main(String[] args) {
+        System.out.println("Normal: " + sum(5));
+        System.out.println("Recursive:" + sumRec(5));
+
+    }
+
+    static int sum(int x) {
+        int sum = 0;
+        for(int i = x; i > 0; i--) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    static int sumRec(int x) {
+        if(x <= 1) {
+            return 1;
+        }
+        return x + sumRec(x-1);
+    }
 }
