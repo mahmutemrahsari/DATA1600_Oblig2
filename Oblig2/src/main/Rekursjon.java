cc22b8ce401f3dfffd72b4696f4044f6819b3515
sarime <sarime@student.matnat.ifi.no>
2020-02-27 03:11:53 +0100

    Oppgave 1.1, 1.2 og 1.3 er ferdig!

--- Oblig2/src/main/Rekursjon.java
+++ Oblig2/src/main/Rekursjon.java
@@ -5,12 +5,23 @@ public class Rekursjon {
     //Test
     public static void main(String[] args) {
         //1.1
-        System.out.println("Normal: " + sum(5));
-        System.out.println("Recursive:" + sumRec(5));
+        System.out.println("Normal sum: " + sum(5));
+        System.out.println("Recursive sum: " + sumRec(5));
 
         //1.2
         System.out.println("POW positive exp recursive: " + pow(4,3));
         System.out.println("POW negative exp recursive: " + pow(4,-3));
+
+        //1.3
+       int a[] = {1,2,4,5,6,7};
+        System.out.println("Normal: ");
+        tabellBaklengs(a);
+
+        System.out.println("\nRecursive: ");
+        tabellBaklengsRec(a,0,a.length-1);
+        for(int i = 0; i<a.length;i++){
+            System.out.print(a[i]+ " ");
+        }
 
     }
 
@@ -56,4 +67,40 @@ public class Rekursjon {
         //base, når expo er lik null
         return 1;
     }
+
+    //1.3
+    //Implementer en metode som skriver ut verdiene i en tabell baklengs. Bruk rekursjon.
+    //Normal løsning
+    static void tabellBaklengs(int a[]){
+        // int a[] = {1,2,4,5,6,7};
+        for(int i = 0; i<a.length; i++){
+            System.out.print(a[a.length-i-1] + " ");
+        }
+    }
+
+    //Rekursiv løsning
+    static void tabellBaklengsRec(int a[], int start, int end){
+        //Base
+        if(start >= end)
+        return;
+
+        //Swapping logic
+        int tmp;
+        tmp = a[start];
+        a[start] = a[end];
+        a[end] = tmp;
+
+        //Rekursiv kall
+        tabellBaklengsRec(a,start+1,end-1);
+
+    }
+
+
+    //
+    //1.4
+    //Implementer en metode som finner den minste verdien i en heltallstabell. Bruk rekursjon.
+    //
+    //1.5
+    //Implementer en metode som søker etter et gitt heltall i en heltallstabell. Bruk rekursjon.
+    //Returner posisjonen til verdien i tabellen eller -1 hvis verdien ikke er i tabellen.
 }
